7f18c033b1fa86e93e69c2f13497123375d2d8f4
paulinasilveira <paulina.silveira@correo.ucu.edu.uy>
2025-09-01 10:04:05 -0300

    Agrego Dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
 namespace Ucu.Poo.Restaurant
 {
 
@@ -6,5 +10,15 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Dish
     {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool IsVegetarian { get; set; }
+
+        public Dish(string name, float price, bool isVegetarian)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.IsVegetarian = isVegetarian;
+        }
     }
 }
